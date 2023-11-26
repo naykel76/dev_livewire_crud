0a6a5e1dda89a118eb98d9fc8254dae75aca77bc
Nathan <nathan@naykel.com.au>
2023-11-27 06:39:19 +1000

    install gotime

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,28 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Nathan Watts',
+            'email' => 'nathan@naykel.com.au',
+            'password' => bcrypt('1'),
+            'email_verified_at' => now(),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Jimmy Peters',
+            'email' => 'jimmy@example.com',
+            'password' => bcrypt('1'),
+            'email_verified_at' => now(),
+        ]);
+
+        User::factory(10)->create();
     }
 }
